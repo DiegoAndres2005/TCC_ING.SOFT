9ddd3ce744f6b613b35385d821974f1d96d8c049
1luisAlejandro5 <luismarttinez15@gmail.com>
2025-11-17 15:43:24 -0500

    Add files via upload

---
+++ api_proyect/ApiProyectApplication.java
@@ -0,0 +1,11 @@
+package com.universidadcartagena.api_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProyectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProyectApplication.class, args);
+    }
+}
